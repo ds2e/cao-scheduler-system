a7e854181a82365a98bf14a516214a80efe606b5
tnguye12 <1996tungnt@gmail.com>
2025-07-17 16:20:50 +0200

    test route

--- resources/js/components/Navbar/MainAppNavbar.jsx
+++ resources/js/components/Navbar/MainAppNavbar.jsx
@@ -77,7 +77,7 @@ export default function MainWebNavbar() {
                             url === '/login' ?
                                 <></>
                                 :
-                                <Link href='/login' className="cursor-pointer flex w-full justify-center rounded-md bg-theme-secondary ml-3 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-theme-secondary-highlight focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                <Link href={route('login')} className="cursor-pointer flex w-full justify-center rounded-md bg-theme-secondary ml-3 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-theme-secondary-highlight focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                     Login
                                 </Link>
                         }
