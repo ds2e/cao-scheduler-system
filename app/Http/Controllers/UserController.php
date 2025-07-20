a61f8bcd31a67fc035fab8e3bac6f0801ad0671b
tnguye12 <1996tungnt@gmail.com>
2025-07-20 16:31:43 +0200

    change something

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -191,7 +191,6 @@ class UserController extends Controller
                 ]);
             })(),
 
-            
             // View specific to Admin and SuperAdmin
             UserRoles::Admin, UserRoles::SuperAdmin => (function () use ($user) {
                 $users_num = User::count();
